92d0f58b4c5090896e10472cc867b18176814486
cm3z4 <m4tchb0x87@gmail.com>
2018-10-01 19:35:04 -0500

    Implemented the Wiki API and touched up the theme. Ready for testing.

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -3,52 +3,86 @@ $(document).ready(function () {
     $(document.body).on("click", "#add-vid", function (event) {
         event.preventDefault();
 
-        var apikey = "AIzaSyCWG4gCwFSmWaI4si9ItKsSBHtA80xMnEk";
-        var keyword = $("#vid-input").val().trim();
-        var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=" + keyword + "type=video&fields=etag%2Citems%2Ckind%2CnextPageToken%2CpageInfo%2CregionCode&key=" + apikey;
+        var wikiSearch = $("#vid-input").val().trim();
+        console.log(wikiSearch);
 
-        if (keyword === "") {
-            return keyword;
-        }
+        if (wikiSearch !== "") {
 
-        // Create an AJAX call
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-            var item = response.items;
-            console.log(response);
-            console.log(response.items);
-            console.log(item[0].snippet);
-            console.log(item[0].snippet.title);
+            // Wiki API call
 
-         var newDiv = $("<div class='vid-results'>");
+            var wikiQueryURL = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + wikiSearch + "&limit=4&format=json";
 
-         $("#vid-view").empty();
-        for (var i = 0; i < 5; i++) {
-    // JSON path
-        var vidTitle = item[i].snippet.title;
-        var vidURL = item[i].id.videoId;
-        var vidDes = item[i].snippet.description;
+            // Create an AJAX call using jsonp to bypass CORS.
+            $.ajax({
+                url: wikiQueryURL,
+                dataType: "jsonp",
+                success: function (wiki) {
 
-    // Append gifs inside of a card body with template literals
-        newDiv.append(`
-            <div class="col s12 m7">
-                <div class="card horizontal">
-                  <div class="card-image">
-                    <iframe allow="fullscreen" width="450" height="300" src="https://www.youtube.com/embed/${vidURL}"></iframe>
-                  </div>
-                  <div class="card-stacked">
-                    <div class="card-content">
-                        <h6><strong>Title:</strong> ${vidTitle}</h6>
-                    <br>
-                        <p><strong>Description:</strong> ${vidDes}</p>
-                    </div>
-                  </div>
-                </div>
-            </div>`);
+                    var wikiTitle = wiki[2];
+                    var wikiDiv = $("<div class='wikiData'>");
+
+                    console.log(wiki);
+
+                    wikiDiv.append(`
+                            <div class="col s12 m7">
+                                <div class="card horizontal">
+                                    <div id="wikiText">
+                                        <p>${wikiTitle}</p>
+                                    </div>
+                                </div>
+                            </div>`);
+                    $("#wikiCont").html(wikiDiv);
+                    $("#vid-input").val("");
+                }
+            });
+
+            var apikey = "AIzaSyCWG4gCwFSmWaI4si9ItKsSBHtA80xMnEk";
+            var keyword = $("#vid-input").val().trim();
+            var tubeQueryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=" + keyword + "type=video&fields=etag%2Citems%2Ckind%2CnextPageToken%2CpageInfo%2CregionCode&key=" + apikey;
+
+            if (keyword === "") {
+                return keyword;
             }
-        $("#vid-view").append(newDiv);
-        })
+
+            // Create an AJAX call.
+            $.ajax({
+                url: tubeQueryURL,
+                method: "GET"
+            }).then(function (response) {
+                var item = response.items;
+                console.log(response);
+                console.log(response.items);
+                console.log(item[0].snippet);
+                console.log(item[0].snippet.title);
+
+                var newDiv = $("<div class='vid-results'>");
+
+                $("#vid-view").empty();
+                for (var i = 0; i < 5; i++) {
+                    // JSON path
+                    var vidTitle = item[i].snippet.title;
+                    var vidURL = item[i].id.videoId;
+                    var vidDes = item[i].snippet.description;
+
+                    // Append gifs inside of a card body with template literals
+                    newDiv.append(`
+                        <div class="col s12 m7">
+                            <div class="card horizontal">
+                                <div class="card-image">
+                                    <iframe allow="fullscreen" width="450" height="300" src="https://www.youtube.com/embed/${vidURL}"></iframe>
+                                </div>
+                                <div class="card-stacked">
+                                    <div class="card-content">
+                                        <h6><strong>Title:</strong> ${vidTitle}</h6>
+                                    <br>
+                                        <p><strong>Description:</strong> ${vidDes}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>`);
+                }
+                $("#vid-view").append(newDiv);
+            });
+        }
     });
 });
